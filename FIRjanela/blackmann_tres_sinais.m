53b5681f0caa4589d2559db0273128fd687692be
Lucas Costa Fontes <lucas.cf11@aluno.ifsc.edu.br>
2025-02-13 15:19:26 -0300

    Finalizando janela

--- FIRjanela/blackmann_tres_sinais.m
+++ FIRjanela/blackmann_tres_sinais.m
@@ -1,18 +1,15 @@
 % 4. Crie um sinal de entrada composto de três componentes senoidais, nas frequências 770 Hz, 852 Hz e 941
-# Hz, com Ωs = 8 kHz. Projete três filtros passa-faixa digitais, o primeiro com frequência central em 770 Hz, o
-# segundo em 852 Hz e o terceiro em 941 Hz. Para o primeiro filtro, as extremidades das faixas de rejeição estão
-# nas frequências 697 e 852; para o segundo, em 770 e 941 Hz; para o terceiro. Em 852 e 1209 Hz. Nos três
-# filtros, a atenuação mínima na faixa de rejeição é 60 dB. 
+# Hz, com Ωs = 8 kHz. Projete três filtros passa-faixa digitais, Para separar essas frequências. 
 
 clear all; clc; close all;
-M = 71;
+M = 201;
 
-Omega_c1 = 697;
-Omega_c2 = 852;
-Omega_c3 = 770;
-Omega_c4 = 941;
-Omega_c5 = Omega_c2;
-Omega_c6 = 1209;
+Omega_c1 = 740;
+Omega_c2 = 800;
+Omega_c3 = 822;
+Omega_c4 = 882;
+Omega_c5 = 911;
+Omega_c6 = 971;
 Omega_s = 8000;
 
 wc1 = Omega_c1*2*pi/Omega_s;
@@ -31,22 +28,22 @@ h1_n(((M-1)/2)+1) = (wc2 - wc1)/pi; %resposta ao impulso para n=0
 h2_n(((M-1)/2)+1) = (wc4 - wc3)/pi; %resposta ao impulso para n=0
 h3_n(((M-1)/2)+1) = (wc6 - wc5)/pi; %resposta ao impulso para n=0
 
-w_black = 0.42+0.5*cos(2*n.*pi/(M))+0.08*cos(4*n.*pi/(M)); %coeficientes da janela de blackman
-h1_black = w_black.*h1_n;
-h2_black = w_black.*h2_n;
-h3_black = w_black.*h3_n;
+
+h1_ret = h1_n;
+h2_ret = h2_n;
+h3_ret = h3_n;
 
 figure
-freqz(h1_black,1);
-title('Filtro FIR passa-banda - Janela de Blackman')
+freqz(h1_ret,1);
+title('Filtro FIR passa-banda - Janela de retangular')
 
 figure
-freqz(h2_black,1);
-title('Filtro FIR passa-banda - Janela de Blackman')
+freqz(h2_ret,1);
+title('Filtro FIR passa-banda - Janela de retangular')
 
 figure
-freqz(h3_black,1);
-title('Filtro FIR passa-banda - Janela de Blackman')
+freqz(h3_ret,1);
+title('Filtro FIR passa-banda - Janela de retangular')
 
 %% Sinal
 tmin = 0;
@@ -62,20 +59,20 @@ S = abs(2*S/L);
 S = fftshift(S);
 freq = Fs*(-(L/2):(L/2)-1)/L;
 
-s_f_h1_black = filter(h1_black,1,s);
-S_F_h1_black = fft(s_f_h1_black);
-S_F_h1_black = abs(2*S_F_h1_black/L);
-S_F_h1_black = fftshift(S_F_h1_black);
+s_f_h1_ret = filter(h1_ret,1,s);
+S_F_h1_ret = fft(s_f_h1_ret);
+S_F_h1_ret = abs(1.8*S_F_h1_ret/L);
+S_F_h1_ret = fftshift(S_F_h1_ret);
 
-s_f_h2_black = filter(h2_black,1,s);
-S_F_h2_black = fft(s_f_h2_black);
-S_F_h2_black = abs(2*S_F_h2_black/L);
-S_F_h2_black = fftshift(S_F_h2_black);
+s_f_h2_ret = filter(h2_ret,1,s);
+S_F_h2_ret = fft(s_f_h2_ret);
+S_F_h2_ret = abs(1.8*S_F_h2_ret/L);
+S_F_h2_ret = fftshift(S_F_h2_ret);
 
-s_f_h3_black = filter(h3_black,1,s);
-S_F_h3_black = fft(s_f_h3_black);
-S_F_h3_black = abs(2*S_F_h3_black/L);
-S_F_h3_black = fftshift(S_F_h3_black);
+s_f_h3_ret = filter(h3_ret,1,s);
+S_F_h3_ret = fft(s_f_h3_ret);
+S_F_h3_ret = abs(1.8*S_F_h3_ret/L);
+S_F_h3_ret = fftshift(S_F_h3_ret);
 
 %% Gráficos do sinal
 figure(4)
@@ -91,7 +88,7 @@ xlabel('f (Hz)')
 ylabel('|S(f)|')
 xlim([-1000 1000])
 
-subplot(3,1,3),plot(freq,S_F_h1_black)
+subplot(3,1,3),plot(freq,S_F_h1_ret)
 title('Espectro de Amplitude do sinal Filtrado ')
 xlabel('f (Hz)')
 ylabel('|S(f)|')
@@ -111,7 +108,7 @@ xlabel('f (Hz)')
 ylabel('|S(f)|')
 xlim([-1000 1000])
 
-subplot(3,1,3),plot(freq,S_F_h2_black)
+subplot(3,1,3),plot(freq,S_F_h2_ret)
 title('Espectro de Amplitude do sinal Filtrado ')
 xlabel('f (Hz)')
 ylabel('|S(f)|')
@@ -130,7 +127,7 @@ xlabel('f (Hz)')
 ylabel('|S(f)|')
 xlim([-1000 1000])
 
-subplot(3,1,3),plot(freq,S_F_h3_black)
+subplot(3,1,3),plot(freq,S_F_h3_ret)
 title('Espectro de Amplitude do sinal Filtrado ')
 xlabel('f (Hz)')
 ylabel('|S(f)|')
